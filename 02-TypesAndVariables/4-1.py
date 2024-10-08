afa1187e867dc9604c49156f55fa28583a62cfda
neo-michael <s222074@student.uek.krakow.pl>
2024-10-08 14:11:39 +0200

    Sekcja 4, zad: 1-3

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Displaying student's personal data
+#
+name = "Adam"
+age = 1
+height = 10
+
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age + 6} years old.")
